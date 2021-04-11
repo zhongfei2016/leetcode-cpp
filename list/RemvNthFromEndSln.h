4dfdc22c77564aacc6459e8c5a655b64c132bfec
unknown <zhongfei1217@163.com>
2021-04-11 22:42:36 +0800

    update

--- list/RemvNthFromEndSln.h
+++ list/RemvNthFromEndSln.h
@@ -1,6 +1,6 @@
 //
 // Created by ZHONGFEI on 2021/4/11.
-//
+// 快慢指针 删除链表的倒数第 N 个结点 https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
 
 #ifndef LEETCODE_CPP_REMVNTHFROMENDSLN_H
 #define LEETCODE_CPP_REMVNTHFROMENDSLN_H
